5c30e461c61bfb6c446fd2e9916d84999ae3e4e9
mauriciorey03 <edgarmeza909@gmail.com>
2023-09-08 13:46:47 -0500

    Estudio en casa

---
+++ Ejercicios/CV/reducc.py
@@ -0,0 +1,21 @@
+# valor=int(input())
+
+# contador=0
+
+# while valor > 1:
+#     contador+=1
+#     if valor % 2 == 0:
+#         new=valor/2
+#     elif valor %2 !=0:
+#         new=valor+1
+#     valor=int(new)
+
+#     print(contador,valor)
+
+valor=int(input())
+lista=[]
+
+for i in range(valor):
+
+    if i % valor==1:
+        lista
